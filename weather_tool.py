04cd03755bc59a8a43e42a2a291584ee3ec486d4
cesarverapy <cv07746@gmail.com>
2024-08-23 17:54:06 -0400

    added a function to fetch the weather data from an api using the location provided by the user

--- weather_tool.py
+++ weather_tool.py
@@ -8,3 +8,18 @@ def parse_Arguments():
     parser.add_argument("place", help="specify the location as 'city-country' (example: Asuncion-PY)")
     parser.add_argument("--output", choices=['json', 'csv', 'plain'], default='plain', help="choose the format for the output")
     return parser.parse_args()
+
+def fetch_weather_Data(city_name, country_code, api_key):
+    endpoint = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}, {country_code}, {api_key} & units = metric"
+    try:
+        result = requests.get(endpoint)
+        result.raise_for_status()
+        return result.json()
+    
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+    
+    return None
